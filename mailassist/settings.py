35e2a1bb84c9f7ae71749e9f0053ebe0c3b4547c
Ramkumar <ram.bit.mtcs@gmail.com>
2017-01-29 05:25:19 +0530

    make it better

--- mailassist/settings.py
+++ mailassist/settings.py
@@ -85,8 +85,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request'
-                'django.template.context_processors.static'
+                'django.core.context_processors.request',
+                'django.core.context_processors.static',
             ],
         },
     },
